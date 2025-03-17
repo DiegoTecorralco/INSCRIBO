6bddc1cfdfd254adf436ee1c20e1267c0cc76de0
Marcos <230733@utxicotepec.edu.mx>
2025-03-16 23:38:48 -0600

    FronEnd

---
+++ FrontEnd/AccesControl/src/app/components/breadcumb/breadcumb.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcumb',
+  imports: [],
+  templateUrl: './breadcumb.component.html',
+  styleUrl: './breadcumb.component.css'
+})
+export class BreadcumbComponent {
+
+}
