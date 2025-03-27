351c16287cdd25a66de2e64375894ecc0b97a5ee
Marcos <230733@utxicotepec.edu.mx>
2025-03-27 07:54:29 -0600

    ISloggedIn

--- FrontEnd/AccesControl/src/app/app.component.ts
+++ FrontEnd/AccesControl/src/app/app.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router'; // Importa RouterModule
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { SigInComponent } from './components/sig-in/sig-in.component';
-import { routes } from './app.routes'; // Importa las rutas
+import { RouterModule, RouterOutlet } from '@angular/router'; 
 import {HeaderComponent} from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true, // Asegúrate de que el componente sea standalone
-  imports: [RouterOutlet, RouterModule,HeaderComponent,FooterComponent], // Importa RouterModule
+  imports: [RouterOutlet, RouterModule,HeaderComponent,FooterComponent,NavbarComponent], // Importa RouterModule
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'AccesControl';
-  isloggedIn : boolean = false;
-   
+  isloggedIn : boolean = false;  
 }
--- FrontEnd/AccesControl/src/app/components/navbar/navbar.component.ts
+++ FrontEnd/AccesControl/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports:[CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  showMobileMenu = false;
+  showProductMenu = false;
 
+  toggleMobileMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+    if (!this.showMobileMenu) {
+      this.showProductMenu = false; // Cierra el submenú al cerrar el menú principal
+    }
+  }
+
+  toggleProductMenu() {
+    this.showProductMenu = !this.showProductMenu;
+  }
+
+  // Cierra el menú al hacer clic fuera
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.mobile-menu-container') && 
+        !target.closest('button[aria-label="Open main menu"]') &&
+        this.showMobileMenu) {
+      this.toggleMobileMenu();
+    }
+  }
 }
