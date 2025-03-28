46084e7672bb23cd8a55e376a8635a275799b242
Marcos <230733@utxicotepec.edu.mx>
2025-03-28 17:18:07 -0600

    Mejorando la paguina de incio para los logeados

--- FrontEnd/AccesControl/src/app/components/dashboard/dashboard.component.ts
+++ FrontEnd/AccesControl/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 // src/app/components/dashboard/dashboard.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 
@@ -9,6 +9,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-  constructor(public authService: AuthService) {}
+export class DashboardComponent implements OnInit  {
+  teacherData: any;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.currentTeacher.subscribe(teacher => {
+      this.teacherData = teacher;
+      
+      if (!teacher) {
+        const storedTeacher = this.authService.getFromStorage('teacher');
+        if (storedTeacher) {
+          this.teacherData = JSON.parse(storedTeacher);
+        } else {
+          this.authService.clearSession();
+          // Redirigir a login si es necesario
+        }
+      }
+    });
+  }
 }
