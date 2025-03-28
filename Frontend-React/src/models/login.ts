d530b51e1605762331d08c4808d3ef9a89c5d294
agustin963 <230365@utxicotepec.edu.mx>
2025-03-28 16:27:25 -0600

    frontend conectado a ala api

--- Frontend-React/src/models/login.ts
+++ Frontend-React/src/models/login.ts
@@ -1,4 +1,4 @@
-export interface {
+export interface teacher{
     student_id:number;
     name:String;
     lastname:String;
---
+++ Frontend-React/src/services/apiInscribo.tsx
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { Asistencia } from '../models/asistencia'
+const URL_API_ASISTENCIA = "http://192.168.156.95:6000/api/asistencia/"
+
+
+    export const getAsistencia = async () => {
+        try {
+            const response = await axios.get(`${URL_API_ASISTENCIA}getAll`);
+            console.log("📥 Datos recibidos de la API:", response.data);
+            return response.data.data as Asistencia[];
+        } catch (error) {
+            console.error("Error en la llamada a la API:", error.response ? error.response.data : error.message);
+            throw new Error("No se pudo conectar con el servidor o la estructura de la respuesta no es válida");
+        }
+    };
